5cf87dd04eee18525c293017c0fe9966fe4895fa
MariiaSenchenko <61646046+MariiaSenchenko@users.noreply.github.com>
2020-03-01 11:27:22 +0200

    Initial commit

---
+++ src/com/it/task.java
@@ -0,0 +1,68 @@
+package com.it;
+
+public class task {
+
+    public static void main(String[] args) {
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        String name = scanner.nextLine();
+//        System.out.println("Hi, " + name);
+
+
+//        for (int i = args.length ; i >= 0; i--) {
+//            System.out.println(i);
+//
+//        }
+
+
+//        for (int a = 0 ; a < 3 ; a++) {
+//            double b = Math.random() * 3;
+//            System.out.print(b + " ");
+//        }
+
+//        for (int a = 0 ; a < 3 ; a++) {
+//            double b = Math.random() * 3;
+//            System.out.println(b + " ");
+//        }
+//
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        String password = scanner.nextLine();
+//        String password2 = "123456";
+//
+//            if (password.equals(password2)) {
+//                System.out.println(password + " = 123456");
+//            } else {
+//                System.out.println("Wrong password");
+//            }
+
+
+//        int sum = 0;
+//        for (int i=0; i<args.length; i++) {
+//            int arg = Integer.parseInt(args[i]);
+//            sum=sum+arg;
+//        }
+//        System.out.println(sum);
+
+
+//        int fact = 1;
+//        for (int i=0; i<args.length; i++) {
+//            int arg = Integer.parseInt(args[i]);
+//            fact=fact * arg;
+//        }
+//        System.out.println(fact);
+//
+        
+
+//        String Str1 = new String("Доброе утро");
+//        String Str2 = new String( "Спокойной ночи");
+//        System.out.print ("Длина строки:" + " Доброе утро" + " -");
+//        System.out.println(Str1.length());
+//        System.out.print ("Длина строки:" + " Спокойной ночи" + " -");
+//        System.out.println(Str2.length());
+
+    }
+
+}
